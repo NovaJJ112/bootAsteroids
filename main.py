85cbae56a568d65bd03e95fb07553c65a2cc8bf0
NovaJJ112 <joshjones@novagroup.co.uk>
2026-08-14 09:28:21 +0100

    2.4 Delta Time

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ import pygame
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type== pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
         screen.fill("black")
         pygame.display.flip()
 
